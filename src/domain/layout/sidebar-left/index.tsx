c095076adf25ae01aa67f2c1b2577efe99a78d4a
masudrahman-dev <masudrahman.dev@gmail.com>
2024-03-02 20:26:37 +0600

    sidebar makes done

--- src/domain/layout/sidebar-left/index.tsx
+++ src/domain/layout/sidebar-left/index.tsx
@@ -64,10 +64,47 @@ const SidebarLeft = () => {
   };
 
   return (
-    <div className="">
-      <div className="border w-72 p-3 h-full flex flex-col gap-3">
-        <div className="flex flex-col gap-5">
+    <div className="w-80 bg-gray-50  overflow-y-auto lg:block">
+      <div className=" w-72 p-3 h-full flex flex-col gap-3">
+        <div className="flex flex-col  gap-5">
           {[...Array(8)].map((_, index) => (
+            <CollapsibleWrapper
+              key={index}
+              collapsibleKey={`collapsible${index + 1}`}
+              // @ts-ignore
+              isOpen={collapsibleStates[`collapsible${index + 1}`]}
+              onStateChange={handleCollapsibleStateChange}
+            >
+              <p>Content for Collapsible {index + 1}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+                porro iste .
+              </p>
+            </CollapsibleWrapper>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-5 mt-12">
+          {[...Array(2)].map((_, index) => (
+            <CollapsibleWrapper
+              key={index}
+              collapsibleKey={`collapsible${index + 1}`}
+              // @ts-ignore
+              isOpen={collapsibleStates[`collapsible${index + 1}`]}
+              onStateChange={handleCollapsibleStateChange}
+            >
+              <p>Content for Collapsible {index + 1}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+                porro iste .
+              </p>
+            </CollapsibleWrapper>
+          ))}
+        </div>
+        <div className="h-full"></div>
+
+        <div className="flex flex-col gap-5">
+          {[...Array(2)].map((_, index) => (
             <CollapsibleWrapper
               key={index}
               collapsibleKey={`collapsible${index + 1}`}
