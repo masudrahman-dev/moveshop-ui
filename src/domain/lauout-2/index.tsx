48f7fa7dcef004706cfe9600e0e018161de9b01e
masudrahman-dev <masudrahman.dev@gmail.com>
2024-03-02 21:54:26 +0600

    test

--- src/domain/lauout-2/index.tsx
+++ src/domain/lauout-2/index.tsx
@@ -4,13 +4,13 @@ const Layout2 = () => {
   return (
     <div>
       <div className="flex h-screen overflow-hidden bg-gray-100">
-        <div className="w-80 bg-gray-50  overflow-y-auto lg:block">
-          <div className=" w-72 p-3 h-full flex flex-col gap-3">
-            {[...Array(10)]?.map(() => {
+        <div className="w-80 bg-gray-50 border  overflow-y-auto lg:block">
+          <div className=" w-72 h-full flex flex-col gap-3 ">
+            {[...Array(10)]?.map((_, i) => {
               return (
                 <p>
-                  <a href="#" className="block p-4 hover:bg-gray-700">
-                    Settings
+                  <a href="#" className="block p-4 hover:bg-gray-300">
+                    Item {i}
                   </a>
                 </p>
               );
@@ -25,11 +25,11 @@ const Layout2 = () => {
                 <h1 className="text-2xl font-semibold">Dashboard</h1>
                 <p>Welcome to the admin dashboard.</p>
 
-                {[...Array(230)]?.map(() => {
+                {[...Array(30)]?.map((_, i) => {
                   return (
                     <p>
-                      <a href="#" className="block p-4 hover:bg-gray-700">
-                        Settings
+                      <a href="#" className="block p-4 hover:bg-gray-300">
+                        item {i}
                       </a>
                     </p>
                   );
