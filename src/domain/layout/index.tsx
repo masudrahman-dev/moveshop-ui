0b87e0a6a537a33d80e052d3b29658f9057cc8c3
masudrahman-dev <masudrahman.dev@gmail.com>
2024-03-03 10:21:32 +0600

    need to fix right sidebar

--- src/domain/layout/index.tsx
+++ src/domain/layout/index.tsx
@@ -19,21 +19,37 @@ const Layout: React.FC = () => {
 
         <div className="flex flex-col flex-1 w-0 overflow-hidden">
           <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
-            <div className="py-6">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                {/* Your page content goes here */}
-                <h1 className="text-2xl font-semibold">Dashboard</h1>
-                <p>Welcome to the admin dashboard.</p>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              <div className="">
+                <div className="">
+                  <h1 className="text-2xl font-semibold">Dashboard</h1>
+                  <p>Welcome to the admin dashboard.</p>
 
-                {[...Array(230)]?.map(() => {
-                  return (
-                    <li>
-                      <a href="#" className="block p-4 hover:bg-gray-700">
-                        Settings
-                      </a>
-                    </li>
-                  );
-                })}
+                  {[...Array(8)]?.map(() => {
+                    return (
+                      <li>
+                        <a href="#" className="block p-4 hover:bg-gray-700">
+                          Settings
+                        </a>
+                      </li>
+                    );
+                  })}
+                </div>
+                <div className="pt-6 col-span-4 border">
+                  <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                    <h1 className="text-2xl font-semibold">Right Sidebar</h1>
+
+                    {[...Array(10)]?.map((_, i) => {
+                      return (
+                        <p>
+                          <a href="#" className="block p-4 hover:bg-gray-700">
+                            item {i}
+                          </a>
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
           </main>
