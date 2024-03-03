bd9df22d91ecb066efc922e0b415c22a6639d6de
masudrahman-dev <masudrahman.dev@gmail.com>
2024-03-03 23:43:39 +0600

    sidebar issue fiexd

--- src/domain/lauout-4/index.tsx
+++ src/domain/lauout-4/index.tsx
@@ -18,45 +18,46 @@ const Layout4 = () => {
       </div>
 
       <div>
-        <aside
-          className={clx(
-            "fixed top-0  left-0 z-40 w-64 h-screen bg-white overflow-y-auto overflow-hidden pt-14 transition-transform -translate-x-full  border-r md:translate-x-0",
-            {
-              "translate-x-0": isOpen,
-            }
-          )}
-        >
-          <SidebarLeft />
-        </aside>
+        <div>
+          <aside
+            className={clx(
+              "fixed top-0 px-1  left-0 z-40 w-64 h-screen bg-white overflow-y-auto pt-14 transition-transform -translate-x-full  border-r md:translate-x-0",
+              {
+                "translate-x-0": isOpen,
+              }
+            )}
+          >
+            <SidebarLeft />
+          </aside>
+          <div className={clx("mt-16 md:ml-64 ml-0")}>
+            <div className="grid grid-cols-1 md:grid-cols-12 justify-items-center">
+              <div className="md:col-span-9">
+                <p>Main Content</p>
 
-        <div className={clx("mt-16 md:ml-64 ml-0")}>
-          <div className="grid grid-cols-1 md:grid-cols-12 justify-items-center">
-            <div className="md:col-span-9">
-              <p>Main Content</p>
+                {[...Array(30)]?.map((_, i) => {
+                  return (
+                    <p key={i}>
+                      <a href="#" className="block p-4 hover:bg-gray-300">
+                        item {i}
+                      </a>
+                    </p>
+                  );
+                })}
+              </div>
+              <div className="md:col-span-3 border w-full text-center  ">
+                <h1 className="text-2xl font-semibold">Right Sidebar</h1>
 
-              {[...Array(30)]?.map((_, i) => {
-                return (
-                  <p key={i}>
-                    <a href="#" className="block p-4 hover:bg-gray-300">
-                      item {i}
-                    </a>
-                  </p>
-                );
-              })}
-            </div>
-            <div className="md:col-span-3 border w-full text-center  ">
-              <h1 className="text-2xl font-semibold">Right Sidebar</h1>
+                <div className="relative group/video">
+                  <img
+                    className="rounded-lg "
+                    src="https://picsum.photos/300/300"
+                    alt=""
+                  />
 
-              <div className="relative group/video">
-                <img
-                  className="rounded-lg "
-                  src="https://picsum.photos/300/300"
-                  alt=""
-                />
-
-                <button className="outline group-hover/video:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-6 w-6 rounded-full flex flex-col items-center justify-center ">
-                  <PlayMiniSolid />
-                </button>
+                  <button className="outline group-hover/video:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-6 w-6 rounded-full flex flex-col items-center justify-center ">
+                    <PlayMiniSolid />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
